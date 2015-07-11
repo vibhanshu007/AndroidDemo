d01362ac212a794308b11e15e53e9fb107acd081
vibhasnhu007 <vibhanshumail849@gmail.com>
2015-07-11 23:50:15 +0530

    new

--- src/com/example/demo/MyForm.java
+++ src/com/example/demo/MyForm.java
@@ -10,34 +10,30 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class MyForm extends Activity {
-	
+
 
 	Button button;
 	EditText name;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_my_form);
-		
+
 		name=(EditText)findViewById(R.id.et_name);
-		
+
 		button=(Button)findViewById(R.id.Submit_button);
 		button.setOnClickListener(new View.OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
-				
+
 				Intent intent=new Intent(getApplicationContext(), SecondActivity.class);
 				intent.putExtra("name", name.getText().toString());
-				
-				
-				
-				
 				startActivity(intent);
-				
+
 			}
 		});
-		
+
 	}
 }
